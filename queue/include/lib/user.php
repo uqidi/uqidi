c4cc58e1ac1d5ffcd43d763cf417afe36dd7702e
tao.wang1 <wangtao@wangqidiyaodeMacBook-Pro.local>
2016-08-14 11:31:37 +0800

    Http

--- queue/include/lib/user.php
+++ queue/include/lib/user.php
@@ -1,9 +1,4 @@
 <?php
 class Lib_User{
-    public static function getUserByUid($uid){
-        $User = Loader::model('User');
-        $user = $User->where(array('uid'=>$uid))->find();
-        return $user;
-    }
 }
 
